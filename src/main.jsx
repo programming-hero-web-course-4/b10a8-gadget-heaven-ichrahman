5a64c3b32b7d98a770b09a6bc9d71dcb91dc2308
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2024-11-12 22:59:04 +0100

    feat: set up routing structure with Root, Home, Dashboard, and Statistics pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,40 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
+import Statistics from './components/Statistics/Statistics.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    // todo: error page
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: 'dashboard',
+        element: <Dashboard />
+      },
+      {
+        path: 'statistics',
+        element: <Statistics />
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
