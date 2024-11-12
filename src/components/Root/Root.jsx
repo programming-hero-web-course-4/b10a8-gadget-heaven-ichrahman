bd3bb8b271e5d952c81b8f20ccc64bb4d9664b54
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2024-11-12 23:11:38 +0100

    style: set up maximum screen width for responsive layout

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -4,7 +4,7 @@ import Footer from "../Footer/Footer";
 
 const Root = () => {
     return (
-        <div>
+        <div className="max-w-screen-xl mx-auto px-4">
             <Navbar />
             <Outlet />
             <Footer />
