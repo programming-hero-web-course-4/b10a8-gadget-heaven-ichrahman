b902c307b80f0620787f840f14b0b7fe7eda4454
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2024-11-13 00:56:35 +0100

    feat: add Navbar and Banner components

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,15 @@
 const Banner = () => {
     return (
-        <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content text-center">
-                <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">Hello there</h1>
+        <div className="min-h-64 bg-purple-600 text-white pb-60">
+            <div className="hero-content text-center w-9/12 mx-auto">
+                <div className="">
+                    <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold py-6">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1>
                     <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!
                     </p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <button className="btn bg-white rounded-full text-purple-600 py-3 px-6 font-bold shadow-md hover:bg-purple-100 transition-transform transform hover:scale-105">
+                        Shop Now
+                    </button>
                 </div>
             </div>
         </div>
