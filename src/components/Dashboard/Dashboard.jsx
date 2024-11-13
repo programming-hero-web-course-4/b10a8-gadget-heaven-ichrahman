98eadff524b74660a2a622273dc6c1aa356c40b4
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2024-11-13 10:51:36 +0100

    feat: add initial structure for Dashboard, Categories, Devices, and Statistics components
    - Implement Dashboard with header, description, and action buttons
    - Create placeholder components for Categories and Devices
    - Design Statistics with header and description styling

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,17 @@
 
 const Dashboard = () => {
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className="bg-purple-600 text-center py-8">
+            <div>
+                <h3 className="text-white text-[32px] font-bold">Dashboard</h3>
+                <p className="text-white pt-4 pb-8 w-8/12 mx-auto">Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
+                <button className="btn btn-outline rounded-full py-3 px-6 mr-6 text-white">
+                    Cart
+                </button>
+                <button className="btn bg-white rounded-full text-purple-600 py-3 px-6 font-bold shadow-md hover:bg-purple-100 transition-transform transform hover:scale-105">
+                    Wishlist
+                </button>
+            </div>
         </div>
     )
 }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,7 +1,10 @@
 const Statistics = () => {
     return (
-        <div>
-            <h1>Statistics</h1>
+        <div className="bg-purple-600 text-center py-8">
+            <div>
+                <h3 className="text-white text-[32px] font-bold">Statistics</h3>
+                <p className="text-white pt-4 pb-8 w-8/12 mx-auto">Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
+            </div>
         </div>
     );
 };
