0f5d17533c6167da80f673b48e99f09a0dda36ff
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2024-11-17 01:15:43 +0100

    feat: display gadget based on category

--- src/components/Devices/Devices.jsx
+++ src/components/Devices/Devices.jsx
@@ -1,24 +1,65 @@
+import { useContext, useState } from "react";
+import { GadgetContext } from "../Root/Root";
+import { Link } from "react-router-dom";
+
+
 const Devices = () => {
+    const [data] = useContext(GadgetContext);
+    // console.log(data)
+
+    const [activeCategory, setActiveCategory] = useState("All Products");
+
+    const filtereddata = activeCategory === "All Products" ? data : data.filter(item => item.category === activeCategory);
+
+
     return (
         <div className="mt-20">
             <h2 className="text-center text-4xl font-bold mb-12">Explore Cutting-Edge Gadgets</h2>
             <div className="grid grid-cols-3 gap-8">
                 <div className="flex flex-col gap-4">
-                    <button className="btn rounded-full bg-[#09080f0d] text-black font-medium py-2 px-7 w-40">
+                    <button onClick={() => setActiveCategory("All Products")}
+                        className={`btn rounded-full py-2 px-7 w-40 ${activeCategory === "All Products" ? "bg-purple-600 text-white" : "bg-gray-100 text-black"
+                            }`}>
                         All Products
                     </button>
-                    <button className="btn bg-white rounded-full bg-[#09080f0d] text-black font-medium py-2 px-7 w-40">
+                    <button onClick={() => setActiveCategory("Wearables")}
+                        className={`btn rounded-full py-2 px-7 w-40 ${activeCategory === "Wearables" ? "bg-purple-600 text-white" : "bg-gray-100 text-black"
+                            }`}>
                         Wearables
                     </button>
-                    <button className="btn bg-white rounded-full bg-[#09080f0d] text-black font-medium py-2 px-7 w-40">
+                    <button onClick={() => setActiveCategory("Computers")}
+                        className={`btn rounded-full py-2 px-7 w-40 ${activeCategory === "Computers" ? "bg-purple-600 text-white" : "bg-gray-100 text-black"
+                            }`}>
                         Computers
                     </button>
-                    <button className="btn bg-white rounded-full bg-[#09080f0d] text-black font-medium py-2 px-7 w-40">
+                    <button onClick={() => setActiveCategory("Drones")}
+                        className={`btn rounded-full py-2 px-7 w-40 ${activeCategory === "Drones" ? "bg-purple-600 text-white" : "bg-gray-100 text-black"
+                            }`}>
                         Drones
                     </button>
                 </div>
                 <div className="col-span-2">
-                    <h2>Products</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                        {
+                            filtereddata?.map(item =>
+                                <div item={item} key={item.product_id} className="card card-compact shadow-xl">
+                                    <figure>
+                                        <img className="h-[190px]"
+                                            src={item.product_image} />
+                                    </figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title">{item.product_title}</h2>
+                                        <p>Price: ${item.price}</p>
+                                        <div className="card-actions justify-start">
+                                            <Link to={`/devices/${item.product_id}`}><button className="btn bg-white rounded-full bg-[#09080f0d] text-black font-medium py-2 px-7 w-40">
+                                                View Details
+                                            </button></Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,17 +1,29 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
+import { createContext, useEffect, useState } from "react";
 
+export const GadgetContext = createContext();
 
 
 const Root = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('./data.json')
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, [])
+
     return (
         <div className="bg-[#F6F6F6]">
-            <div className="max-w-screen-xl mx-auto px-4">
-                <Navbar />
-                <Outlet />
-                <Footer />
-            </div>
+            <GadgetContext.Provider value={[data]}>
+                <div className="max-w-screen-xl mx-auto px-4">
+                    <Navbar />
+                    <Outlet />
+                    <Footer />
+                </div>
+            </GadgetContext.Provider>
         </div>
     );
 };
